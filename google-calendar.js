cdfccab8e9565c3117f69e114eb3b38aadb6446f
jason <jason@pipedream.com>
2020-04-21 10:55:59 -0700

    this works locally and as a component

--- google-calendar.js
+++ google-calendar.js
@@ -1,11 +1,6 @@
 const { google } = require('googleapis');
 
 let tokens = null
-if (process.env.RUNTIME === "local") {
-  const fs = require('fs')
-  const raw = fs.readFileSync("tokens.json")
-  tokens = JSON.parse(raw)
-}
 
 console.log(process.env.RUNTIME)
 
@@ -41,7 +36,7 @@ const googleCalendar = {
 }
 
 const component = {
-  name: 'calendar',
+  name: 'calendar2',
   version: '0.0.1',
   props: {
     calendarId: {
@@ -51,11 +46,15 @@ const component = {
     timer: {
       type: "$.interface.timer",
       default: {
-        cron: "* * * * *",
+        cron: "0/5 * * * *",
       },
     },
   },
   async run(event) {
+    console.log("this start")
+    console.log(this)
+
+    console.log("this end")
     const now = new Date()
 
     const timeMin = new Date(now.getTime()).toISOString()
@@ -64,21 +63,22 @@ const component = {
     const config = {
       calendarId: this.calendarId,
       timeMin,
-      maxResults: 1,
+//      maxResults: 1,
       singleEvents: true,
       orderBy: 'startTime',
     }
     const resp = await this.googleCalendar.getEvents(config)
 
     if (resp && resp.data && resp.data.items && Array.isArray(resp.data.items) && resp.data.items.length) {
+      this.$emit(resp.data)
       const event = resp.data.items[0]
       const eventStart = new Date(event.start.dateTime)
-      if (eventStart.getTime() - now.getTime() < (1000 * 60 * 10)) {
+/*      if (eventStart.getTime() - now.getTime() < (1000 * 60 * 10)) {
         console.log("emitting data")
         this.$emit(resp.data)
       } else {
         console.log("not emitting since event start time is > than 10 mins from now")
-      }
+      }*/
     } else {
 //      console.log(resp)
       console.log("nothing to emit")
@@ -86,8 +86,30 @@ const component = {
   },
 };
 
-console.log("before run")
-component.run(null).then( a => console.log(a))
-console.log("after run")
+if (process.env.RUNTIME === "local") {
+  const fs = require('fs')
+  const raw = fs.readFileSync("tokens.json")
+
+  // $ stuff
+  component.$emit = (ev) => {
+    console.log("$EMIT START")
+    console.log(ev)
+    console.log("$EMIT END")
+  }
+
+  // app methods // TODO expand methods object
+  googleCalendar._tokens = googleCalendar.methods._tokens
+  googleCalendar.calendar = googleCalendar.methods.calendar
+  googleCalendar.getEvents = googleCalendar.methods.getEvents
+
+
+  // component props TODO expand props
+  component.calendarId = "pipedream.com_8khjp5mfv7t6tdsnnlo85bm18c@group.calendar.google.com"
+  component.googleCalendar = component.props.googleCalendar
+  tokens = JSON.parse(raw)
+  console.log("before run")
+  component.run(null).then( a => console.log(a))
+  console.log("after run")
+}
 
 module.exports = component
